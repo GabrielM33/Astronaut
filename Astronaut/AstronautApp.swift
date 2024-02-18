4ed882556e19fb345a36556fe99c62f3a1fe4825
gabrielm33 <gabrielmoraesweb@gmail.com>
2024-02-18 18:42:18 -0300

    Initial Commit

---
+++ Astronaut/AstronautApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautApp.swift
+//  Astronaut
+//
+//  Created by Gabriel Moraes on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AstronautApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
