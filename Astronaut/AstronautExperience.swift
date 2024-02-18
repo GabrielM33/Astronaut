c4dd8be8f7b6d2b3ec68d03156e1e741f0e64ad3
gabrielm33 <gabrielmoraesweb@gmail.com>
2024-02-18 19:35:20 -0300

    fixed volumes

---
+++ Astronaut/AstronautExperience.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautExperience.swift
+//  Astronaut
+//
+//  Created by Gabriel Moraes on 18/02/24.
+//
+
+import SwiftUI
+
+struct AstronautExperience: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautExperience()
+}
